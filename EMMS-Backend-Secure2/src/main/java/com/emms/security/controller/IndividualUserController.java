4da1f665f759f87335ac843ffda86f08fba042ab
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2021-07-21 12:02:25 +0530

    Added change password method

--- EMMS-Backend-Secure2/src/main/java/com/emms/security/controller/IndividualUserController.java
+++ EMMS-Backend-Secure2/src/main/java/com/emms/security/controller/IndividualUserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.emms.api.IndividualUserApi;
 import com.emms.api.UserApi;
 import com.emms.model.IndividualOldPasswordCheckRequest;
+import com.emms.model.PasswordResetRequest;
 import com.emms.model.UserResponse;
 
 @RestController
@@ -36,6 +38,11 @@ public class IndividualUserController {
 	@PostMapping("checkOldPassword")
 	public boolean checkOldPassword(@RequestBody IndividualOldPasswordCheckRequest passwordCheckRequest) {
 		return individualUserApi.isOldPasswordCorrect(passwordCheckRequest);
+	}
+	
+	@PutMapping("changePassword")
+	public UserResponse changePassword(@RequestBody PasswordResetRequest passwordResetRequest) {
+		return userApi.resetPassword(passwordResetRequest);
 	}
 
 }
